b84f2ef64bfe1876245e06fa45563cf64f32dd53
Will Ruggiano <wmruggiano@gmail.com>
2022-09-29 12:23:46 -0600

    feat: client-stats

--- modules/ethereum/monitoring/prysm/client-stats.nix
+++ modules/ethereum/monitoring/prysm/client-stats.nix
@@ -16,43 +16,52 @@ in {
     validator-metrics-url = mkOption {
       type = types.str;
       description = "The url of the validator instance";
-      default = "127.0.0.1:8081/metrics";
+      default = "http://localhost:8081/metrics";
     };
     beacon-node-metrics-url = mkOption {
       type = types.str;
       description = "The url of the beacon node instance";
-      default = "127.0.0.1:8080/metrics";
+      default = "http://localhost:8080/metrics";
     };
     api-url = mkOption {
       type = types.str;
       description = "The url of the client-stats api url";
+      default = null;
+    };
+    log-format = mkOption {
+      type = types.enum ["text" "json" "fluentd" "journald"];
+      description = "Specify log formatting";
+      default = "journald";
     };
   };
 
-  config = mkIf cfg.enable {
-    systemd.services.client-stats = let
-      inherit (config.services.ethereum.consensus) prysm;
-      dependent-services = (optional prysm.beacon-chain.enable "beacon-chain.service") ++ (optional prysm.validator.enable "validator.service");
-    in {
-      description = "Prysm client stats";
-      enable = true;
-      wantedBy = ["multi-user.target"];
-      after = dependent-services;
-      serviceConfig = {
-        ExecStart = ''
+  config = let
+    inherit (config.services.ethereum.consensus) prysm;
+    dependent-services = (optional prysm.beacon-chain.enable "beacon-chain.service") ++ (optional prysm.validator.enable "validator.service");
+  in
+    mkIf (cfg.enable && (prysm.beacon-chain.enable || prysm.validator.enable)) {
+      systemd.services.client-stats = {
+        description = "Prysm client stats";
+        enable = true;
+        wantedBy = ["multi-user.target"];
+        after = dependent-services;
+        serviceConfig = {
+            DynamicUser = true;
+            Restart = "always";
+            # Hardening:
+            PrivateTmp = true;
+            ProtectSystem = "full";
+            NoNewPrivileges = true;
+            PrivateDevices = true;
+            MemoryDenyWriteExecute = true;
+        };
+        script = ''
           ${cfg.package}/bin/client-stats \
-            --validator-metrics-url ${cfg.validator-metrics-url} \
-            --beacon-node-metrics-url ${cfg.beacon-node-metrics-url} \
-            --clientstats-api-url ${cfg.api-url}
+            ${optionalString prysm.beacon-chain.enable "--beacon-node-metrics-url ${cfg.beacon-node-metrics-url}"} \
+            ${optionalString prysm.validator.enable "--validator-metrics-url ${cfg.validator-metrics-url}"} \
+            ${optionalString (cfg.api-url != null) "--clientstats-api-url ${cfg.api-url}"} \
+            --log-format ${cfg.log-format}
         '';
-        Restart = "always";
       };
-      script = ''
-        ${cfg.package}/bin/client-stats \
-          --clientstats-api-url ${cfg.api-url} \
-          ${optionalString prysm.beacon-chain.enable "--beacon-node-metrics-url ${cfg.beacon-node-metrics-url}"} \
-          ${optionalString prysm.validator.enable "--validator-metrics-url ${cfg.validator-metrics-url}"} \
-      '';
     };
-  };
 }
